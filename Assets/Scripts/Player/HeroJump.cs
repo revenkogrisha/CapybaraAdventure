dc4dfb840b4483ea5a26030d9d07f5ba9c33de56
Grisha Revenko <grisha@revenko.org>
2023-03-15 07:14:01 +0300

    Implemented jump system

--- Assets/Scripts/Player/HeroJump.cs
+++ Assets/Scripts/Player/HeroJump.cs
@@ -6,22 +6,28 @@ namespace CapybaraAdventure.Player
 {
     public class HeroJump : MonoBehaviour
     {
+        [SerializeField] private Rigidbody2D _rigidBody2D;
+        [SerializeField] private float _duration = 1.5f;
         [SerializeField] private AnimationCurve _jumpCurve;
 
         private JumpSlider _jumpSlider;
         private JumpButton _jumpButton;
         private bool _shouldJump = false;
+        private float _expiredJumpTime = 0f;
+        private float _jumpForce;
 
         #region MonoBehaviour
 
         public void OnEnable()
         {
             _jumpButton.OnClicked += SayShouldJump;
+            _jumpButton.OnClicked += UpdateForceValue;
         }
 
         public void OnDisable()
         {
             _jumpButton.OnClicked -= SayShouldJump;
+            _jumpButton.OnClicked -= UpdateForceValue;
         }
         
         private void Update()
@@ -43,6 +49,11 @@ namespace CapybaraAdventure.Player
             _shouldJump = true;
         }
 
+        private void UpdateForceValue()
+        {
+            _jumpForce = _jumpSlider.Value;
+        }
+
         private void TryJump()
         {
             if (_shouldJump)
@@ -51,9 +62,16 @@ namespace CapybaraAdventure.Player
 
         private void PerformJump()
         {
-            Debug.Log("Jump!");
+            _expiredJumpTime += Time.deltaTime;
 
-            _shouldJump = false;
+            if (_expiredJumpTime > _duration)
+                _shouldJump = false;
+
+            var progress = _expiredJumpTime / _duration;
+            var y = _jumpCurve.Evaluate(progress);
+
+            var jumpVelocity = new Vector2(_jumpForce, y * _jumpForce);
+            _rigidBody2D.velocity = jumpVelocity;
         }
     }
 }
