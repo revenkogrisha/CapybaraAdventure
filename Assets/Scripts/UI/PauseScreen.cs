a7d37711ef751c383feaef3dc10b86779b71d501
Grisha Revenko <grisha@revenko.org>
2023-04-04 23:59:59 +0300

    Added pause screen addressable

---
+++ Assets/Scripts/UI/PauseScreen.cs
@@ -0,0 +1,59 @@
+using CapybaraAdventure.Tools;
+using UnityEngine;
+using UnityTools.Buttons;
+using System;
+using CapybaraAdventure.Game;
+
+namespace CapybaraAdventure.UI
+{
+    public class PauseScreen : UIBase
+    {
+        [SerializeField] private UIButton _backToGameButton;
+        [SerializeField] private UIButton _restartButton;
+
+        private PauseManager _pauseManager;
+        private GameUI _gameUI;
+        private RestartGameService _restartService = new();
+
+        private bool isInitialized = false;
+
+        #region MonoBehaviour
+
+        private void OnEnable()
+        {
+            _backToGameButton.OnClicked += ReturnToGame;
+            _restartButton.OnClicked += RestartGame;
+        }
+
+        private void OnDisable()
+        {
+            _backToGameButton.OnClicked -= ReturnToGame;
+            _restartButton.OnClicked -= RestartGame;
+        }
+
+        #endregion
+        
+        public void Init(
+            PauseManager pauseManager,
+            GameUI gameUI)
+        {
+            _pauseManager = pauseManager;
+            _gameUI = gameUI;
+
+            isInitialized = true;
+        }
+
+        private void ReturnToGame()
+        {
+            if (isInitialized == false)
+                throw new InvalidOperationException("The component wasn't initialized! Call Init() first.");
+
+            _pauseManager.SetPaused(false);
+
+            Conceal();
+            _gameUI.Reveal();
+        }
+
+        private void RestartGame() => _restartService.Restart();
+    }
+}
