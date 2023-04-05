6b0f00529d2276f25048492cc2f157599f729e06
Grisha Revenko <grisha@revenko.org>
2023-04-05 18:32:02 +0300

    Added pause button

--- Assets/Scripts/UI/GameUI.cs
+++ Assets/Scripts/UI/GameUI.cs
@@ -1,4 +1,49 @@
+using Zenject;
+using CapybaraAdventure.Tools;
+using UnityEngine;
+using UnityTools.Buttons;
+using CapybaraAdventure.Game;
+
 namespace CapybaraAdventure.UI
 {
-    public class GameUI : UIBase { }
+    public class GameUI : UIBase
+    {
+        [SerializeField] private UIButton _pauseButton;
+
+        private PauseManager _pauseManager;
+        private PauseScreenProvider _pauseScreenProvider;
+
+        #region MonoBehaviour
+
+        private void OnEnable()
+        {
+            _pauseButton.OnClicked += PauseGame;
+        }
+
+        private void OnDisable()
+        {
+            _pauseButton.OnClicked -= PauseGame;
+        }
+
+        #endregion
+
+        [Inject]
+        private void Construct(
+            PauseManager pauseManager,
+            PauseScreenProvider pauseScreenProvider)
+        {
+            _pauseManager = pauseManager;
+            _pauseScreenProvider = pauseScreenProvider;
+        }
+
+        private async void PauseGame()
+        {
+            _pauseManager.SetPaused(true);
+
+            var screen = await _pauseScreenProvider.Load();
+            screen.Init(_pauseManager, this);
+
+            Conceal();
+        }
+    }
 }
