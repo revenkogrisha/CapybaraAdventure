e2ea2699e4c27379e7a051b4481551ca1e670418
Grisha Revenko <grisha@revenko.org>
2023-04-08 22:44:10 +0300

    Renamed particles

---
+++ Assets/Scripts/Player/ParticlesPlayer.cs
@@ -0,0 +1,37 @@
+using NTC.Global.Pool;
+using UnityEngine;
+
+namespace CapybaraAdventure.Player
+{
+    public class ParticlesPlayer : MonoBehaviour
+    {
+        private const float JumpParticlesDespawnDelay = 1f;
+
+        [SerializeField] private ParticleSystem _particlesPrefab;
+        [SerializeField] private Transform _spawnTransform;
+
+        private ParticleSystem _particles;
+        private bool _areParticlesPlaying;
+
+        public void SpawnTillDuration()
+        {
+            if (_areParticlesPlaying == true)
+                return;
+
+            _areParticlesPlaying = true;
+
+            Vector3 spawnPosition = _spawnTransform.position;
+            _particles = NightPool.Spawn(_particlesPrefab, spawnPosition);
+
+            _particles.Play();
+            
+            Invoke(nameof(Despawn), JumpParticlesDespawnDelay);
+        }
+
+        private void Despawn()
+        {
+            NightPool.Despawn(_particles);
+            _areParticlesPlaying = false;
+        }
+    }
+}
