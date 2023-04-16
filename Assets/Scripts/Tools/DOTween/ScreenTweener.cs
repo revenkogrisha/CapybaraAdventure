caf49bdba0e0caf013381ae5d9422b955daca86b
Grisha Revenko <grisha@revenko.org>
2023-04-16 12:32:09 +0300

    Added ScreenTweener for UI screens

---
+++ Assets/Scripts/Tools/DOTween/ScreenTweener.cs
@@ -0,0 +1,55 @@
+using DG.Tweening;
+using UnityEngine;
+
+namespace CapybaraAdventure.Other
+{
+    public class ScreenTweener
+    {
+        private readonly float _logoTargetY = 245f;
+        private readonly float _logoShowDuration = 0.5f;
+        private readonly float _buttonsDelay = 0.3f;
+        private readonly float _buttonsScaleDuration = 0.4f;
+
+        public ScreenTweener() {  }
+
+        public ScreenTweener(float commonShowDuration) : base()
+        {
+            _logoShowDuration = commonShowDuration;
+            _buttonsScaleDuration = commonShowDuration;
+        }
+
+        public void TweenLogo(Transform logo)
+        {
+            logo.localScale = Vector2.zero;
+
+            logo.DOScale(Vector2.one, _logoShowDuration);
+            logo.DOLocalMoveY(_logoTargetY, _logoShowDuration);
+            
+        }
+
+        public void ScaleTweenLogo(Transform logo)
+        {
+            logo.localScale = Vector2.zero;
+
+            logo.DOScale(Vector2.one, _logoShowDuration);
+            
+        }
+
+        public void TweenButton(Transform button)
+        {
+            button.localScale = Vector2.zero;
+
+            DOTween.Sequence()
+                .AppendInterval(_buttonsDelay)
+                .Append(button.DOScale(Vector2.one, _buttonsScaleDuration));
+        }
+
+        public void TweenButtonWithoutDelay(Transform button)
+        {
+            button.localScale = Vector2.zero;
+
+            DOTween.Sequence()
+                .Append(button.DOScale(Vector2.one, _buttonsScaleDuration));
+        }
+    }
+}
