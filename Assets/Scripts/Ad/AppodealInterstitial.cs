0297f6801b347f5387f76df01aba5f848a3d51f5
Grisha Revenko <grisha@revenko.org>
2023-04-28 15:33:57 +0300

    Added TryShowWithChance()

--- Assets/Scripts/Ad/AppodealInterstitial.cs
+++ Assets/Scripts/Ad/AppodealInterstitial.cs
@@ -9,6 +9,8 @@ namespace CapybaraAdventure.Ad
     {
         private readonly string appKey = AppodealStartup.AppKey;
 
+        [SerializeField] private int _showChance = 40;
+
         public bool IsLoaded => Appodeal.IsLoaded(AppodealAdType.Interstitial) == true;
 
         private void Start()
@@ -16,7 +18,14 @@ namespace CapybaraAdventure.Ad
             Appodeal.Initialize(appKey, AppodealAdType.Interstitial, this);
         }
 
-        public void TryShow()
+        public void TryShowWithChance()
+        {
+            int randomChance = Random.Range(0, 101);
+            if (randomChance <= _showChance)
+                Show();
+        }
+
+        public void Show()
         {
             if (IsLoaded == true)
                 Appodeal.Show(AppodealAdType.Interstitial);
