964c4f7747a01f4ecb0d1749660175e846f5af92
Grisha Revenko <grisha@revenko.org>
2023-08-03 15:11:57 +0400

    Updated pregame cutscene

--- Assets/Scripts/Level/Cutscene.cs
+++ Assets/Scripts/Level/Cutscene.cs
@@ -8,12 +8,16 @@ namespace CapybaraAdventure.Level
     public class Cutscene : MonoBehaviour
     {
         private const string Cutscene0 = nameof(Cutscene0);
+        private const string ZoomHero = nameof(ZoomHero);
+        private const string ZoomDark = nameof(ZoomDark);
 
         [SerializeField] private Animator _cutsceneAnimator;
 
         private LoadingScreenProvider _loaderProvider;
-        private readonly float _cutsceneChangeInterval = 2f;
+        private readonly float _cutsceneChangeInterval = 1.5f;
         private int _cutscene0Id = Animator.StringToHash(Cutscene0);
+        private int _zoomHeroId = Animator.StringToHash(ZoomHero);
+        private int _zoomDarkId = Animator.StringToHash(ZoomDark);
 
         private void Start()
         {
@@ -30,7 +34,16 @@ namespace CapybaraAdventure.Level
         {
             yield return new WaitForSeconds(_cutsceneChangeInterval);
 
+            _cutsceneAnimator.SetBool(_zoomHeroId, true);
+
+            yield return new WaitForSeconds(_cutsceneChangeInterval);
+
             _cutsceneAnimator.SetBool(_cutscene0Id, true);
+            _cutsceneAnimator.SetBool(_zoomHeroId, false);
+
+            yield return new WaitForSeconds(_cutsceneChangeInterval);
+
+            _cutsceneAnimator.SetBool(_zoomDarkId, true);
 
             yield return new WaitForSeconds(_cutsceneChangeInterval);
 
