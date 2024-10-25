bdf6a74ee4ddd0da63f7e89bac65d90d706fb2e4
Grisha Revenko <grisha@revenko.org>
2024-10-25 09:35:26 +0400

    📦NEW: add updated UIButton (wip)

---
+++ Assets/Scripts/UI/TweenClickable.cs
@@ -0,0 +1,32 @@
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace CapybaraAdventure.UI
+{
+    public class TweenClickable : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+    {
+        private const float ClickScale = 0.85f;
+        private const float ClickTweenDuration = 0.08f;
+        
+        private bool _isTweeningScale = false;
+        
+        public virtual void OnPointerDown(PointerEventData eventData)
+        {
+            if (_isTweeningScale == false)
+            {
+                _isTweeningScale = true;
+                transform.DOScale(ClickScale, ClickTweenDuration);
+            }
+        }
+
+        public virtual void OnPointerUp(PointerEventData eventData)
+        {
+            if (_isTweeningScale == true)
+            {
+                transform.DOScale(1f, ClickTweenDuration);
+                _isTweeningScale = false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UI/UIButton.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using Core.Audio;
+using DG.Tweening;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using Zenject;
+
+namespace CapybaraAdventure.UI
+{
+    public class UIButton : TweenClickable
+    {
+        private const float LockShakeDuration = 0.3f;
+        private const float LockShakeStrength = 9f;
+        private const int LockShakeVibration = 10;
+    
+        [Header("Settings")]
+        [SerializeField] private bool _playSound = true;
+    
+        [Header("Components")]
+        [SerializeField] private Button _button;
+        [SerializeField] private Image _image;
+        [SerializeField] private TMP_Text _tmp;
+
+        [Header("Graphics")]
+        [SerializeField] private Sprite _normalSprite;
+        [SerializeField] private Sprite _disabledSprite;
+
+        [Space]
+        [SerializeField] private Color _textNormalColor = Color.white;
+        [SerializeField] private Color _textDisabledColor = Color.grey;
+
+        private IAudioHandler _audioHandler;
+
+        public TMP_Text TMP => _tmp;
+
+        public bool Interactable
+        {
+            get => _button.interactable;
+            set
+            {
+                _button.interactable = value;
+                _image.sprite = value == true
+                    ? _normalSprite
+                    : _disabledSprite;
+
+                _tmp.color = value == true
+                    ? _textNormalColor
+                    : _textDisabledColor;
+            }
+        }
+
+        public event Action OnClickStarted;
+        public event Action OnClicked;
+        public event Action OnClickEnded;
+
+        #region MonoBehaviour
+
+        private void OnEnable() => 
+            _button.onClick.AddListener(PerformClick);
+
+        private void OnDisable() => 
+            _button.onClick.RemoveAllListeners();
+
+        #endregion
+
+        [Inject]
+        private void Construct(IAudioHandler audioHandler) =>
+            _audioHandler = audioHandler;
+
+        public override void OnPointerDown(PointerEventData eventData)
+        {
+            if (Interactable == true)
+            {
+                OnClickStarted?.Invoke();
+                base.OnPointerDown(eventData);
+            }
+        }
+
+        public override void OnPointerUp(PointerEventData eventData)
+        {
+            if (Interactable == true)
+            {
+                OnClickEnded?.Invoke();
+                base.OnPointerUp(eventData);
+            }
+            else
+            {
+                Shake();
+            }
+        }
+
+        public void Shake() =>
+            transform.DOShakePosition(LockShakeDuration, LockShakeStrength, LockShakeVibration);
+        
+        public void SetActive(bool active) => 
+            gameObject.SetActive(active);
+
+        private void PerformClick()
+        {
+            if (_audioHandler != null && _playSound == true)
+                _audioHandler.PlaySound(AudioName.Button, true);
+            
+            OnClicked?.Invoke();
+        }
+    }
+}
