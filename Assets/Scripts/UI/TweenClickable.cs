23a74c1eb90c2b2af51430a46c58be5b026d1e59
Grisha Revenko <grisha@revenko.org>
2024-10-27 12:45:30 +0400

    🐛FIX: UI buttons

--- Assets/Scripts/UI/TweenClickable.cs
+++ Assets/Scripts/UI/TweenClickable.cs
@@ -9,22 +9,26 @@ namespace CapybaraAdventure.UI
         private const float ClickScale = 0.85f;
         private const float ClickTweenDuration = 0.08f;
         
+        [SerializeField] private bool _isTweenClickable = true;
+        
         private bool _isTweeningScale = false;
         
         public virtual void OnPointerDown(PointerEventData eventData)
         {
-            if (_isTweeningScale == false)
+            if (_isTweeningScale == false && _isTweenClickable == true)
             {
                 _isTweeningScale = true;
+                Debug.Log(1);
                 transform.DOScale(ClickScale, ClickTweenDuration);
             }
         }
 
         public virtual void OnPointerUp(PointerEventData eventData)
         {
-            if (_isTweeningScale == true)
+            if (_isTweeningScale == true && _isTweenClickable == true)
             {
                 transform.DOScale(1f, ClickTweenDuration);
+                Debug.Log(2);
                 _isTweeningScale = false;
             }
         }
