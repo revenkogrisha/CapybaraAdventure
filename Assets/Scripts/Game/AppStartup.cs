452952d8d1cb5be99429c0d147e34d5026728e1a
Grisha Revenko <grisha@revenko.org>
2025-01-05 16:36:33 +0400

    📦NEW: disable Ingame Debug Console on production builds (non-debug builds)

--- Assets/Scripts/Game/AppStartup.cs
+++ Assets/Scripts/Game/AppStartup.cs
@@ -1,5 +1,6 @@
 using CapybaraAdventure.Other;
 using Core.Common.ThirdParty;
+using IngameDebugConsole;
 using UnityEngine;
 using Zenject;
 
@@ -7,12 +8,18 @@ namespace CapybaraAdventure.Game
 {
     public class AppStartup : MonoBehaviour
     {
+        [SerializeField] private DebugLogManager _ingameDebugLogManager;
+        
         private LoadingScreenProvider _loaderProvider;
 
         private async void Start()
         {
             // NEW
+            if (Debug.isDebugBuild == false)
+                Object.Destroy(_ingameDebugLogManager.gameObject);
+            
             FirebaseService.Initialize();
+            // ––––––––––
             
             Application.targetFrameRate = 60;
 
