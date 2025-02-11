3dd670a961a88d6d9fb5f1abb9ffa6c887660999
Grisha Revenko <grisha@revenko.org>
2025-02-11 10:28:15 +0400

    👌IMPROVE: decrease Jump Slider start lerp speed and max lerp speed slightly

--- Assets/Scripts/UI/JumpSlider.cs
+++ Assets/Scripts/UI/JumpSlider.cs
@@ -9,9 +9,9 @@ namespace CapybaraAdventure.UI
     public class JumpSlider : MonoBehaviour
     {
         private const float LerpSpeedIncrease = 0.05f;
-        private const float MaxLerpSpeed = 0.75f;
+        private const float MaxLerpSpeed = 0.7f;
         private const float MinValue = 3f;
-        private const float StartLerpSpeed = 0.35f;
+        private const float StartLerpSpeed = 0.3f;
 
         [SerializeField] private Slider _slider;
 
